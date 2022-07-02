c8667df903e0f5dcd8b73b2b97380388fe4ddf87
kyowill <yangkai@yangkaideMac-mini.local>
2022-07-02 18:17:00 +0800

    3.2

--- src/java/simpledb/optimizer/TableStats.java
+++ src/java/simpledb/optimizer/TableStats.java
@@ -1,11 +1,14 @@
 package simpledb.optimizer;
 
 import simpledb.common.Database;
+import simpledb.common.DbException;
 import simpledb.common.Type;
 import simpledb.execution.Predicate;
 import simpledb.execution.SeqScan;
 import simpledb.storage.*;
 import simpledb.transaction.Transaction;
+import simpledb.transaction.TransactionAbortedException;
+import simpledb.transaction.TransactionId;
 
 import java.util.HashMap;
 import java.util.Iterator;
@@ -15,8 +18,8 @@ import java.util.concurrent.ConcurrentMap;
 
 /**
  * TableStats represents statistics (e.g., histograms) about base tables in a
- * query. 
- * 
+ * query.
+ * <p>
  * This class is not needed in implementing lab1 and lab2.
  */
 public class TableStats {
@@ -32,9 +35,8 @@ public class TableStats {
     public static void setTableStats(String tablename, TableStats stats) {
         statsMap.put(tablename, stats);
     }
-    
-    public static void setStatsMap(Map<String,TableStats> s)
-    {
+
+    public static void setStatsMap(Map<String, TableStats> s) {
         try {
             java.lang.reflect.Field statsMapF = TableStats.class.getDeclaredField("statsMap");
             statsMapF.setAccessible(true);
@@ -68,15 +70,27 @@ public class TableStats {
      */
     static final int NUM_HIST_BINS = 100;
 
+    private int tableId;
+
+    private int ioCostPerPage;
+
+    private Map<Integer, IntHistogram> intHistogramMap;
+
+    private Map<Integer, StringHistogram> stringHistogramMap;
+
+    private int size;
+
+    static final int MAX_VALUE = 100;
+
+    static final int MIN_VALUE = 1;
+
     /**
      * Create a new TableStats object, that keeps track of statistics on each
      * column of a table
-     * 
-     * @param tableid
-     *            The table over which to compute statistics
-     * @param ioCostPerPage
-     *            The cost per page of IO. This doesn't differentiate between
-     *            sequential-scan IO and disk seeks.
+     *
+     * @param tableid       The table over which to compute statistics
+     * @param ioCostPerPage The cost per page of IO. This doesn't differentiate between
+     *                      sequential-scan IO and disk seeks.
      */
     public TableStats(int tableid, int ioCostPerPage) {
         // For this function, you'll have to get the
@@ -87,78 +101,127 @@ public class TableStats {
         // necessarily have to (for example) do everything
         // in a single scan of the table.
         // some code goes here
+        this.tableId = tableid;
+        this.ioCostPerPage = ioCostPerPage;
+        this.intHistogramMap = new HashMap<>();
+        this.stringHistogramMap = new HashMap<>();
+        DbFile dbFile = Database.getCatalog().getDatabaseFile(tableId);
+        TupleDesc td = dbFile.getTupleDesc();
+
+        // init histogram map
+        Iterator<TupleDesc.TDItem> tdItemIterator = td.iterator();
+        int index = 0;
+        while (tdItemIterator.hasNext()) {
+            TupleDesc.TDItem tdItem = tdItemIterator.next();
+            Type type = tdItem.getFieldType();
+            if (type.equals(Type.INT_TYPE)) {
+                intHistogramMap.put(index, new IntHistogram(NUM_HIST_BINS, MIN_VALUE, MAX_VALUE));
+            } else if (type.equals(Type.STRING_TYPE)) {
+                stringHistogramMap.put(index, new StringHistogram(NUM_HIST_BINS));
+            }
+            index++;
+        }
+        // assign value
+        DbFileIterator iterator = dbFile.iterator(new TransactionId());
+        try {
+            iterator.open();
+            while (true) {
+                if (!iterator.hasNext()) break;
+                Tuple tuple = iterator.next();
+                Iterator<Field> fItr = tuple.fields();
+                int idx = 0;
+                while (fItr.hasNext()) {
+                    if (intHistogramMap.containsKey(idx)) {
+                        IntHistogram intHistogram = intHistogramMap.get(idx);
+                        IntField field = (IntField) fItr.next();
+                        intHistogram.addValue(field.getValue());
+                    } else {
+                        StringHistogram stringHistogram = stringHistogramMap.get(idx);
+                        StringField field = (StringField) fItr.next();
+                        stringHistogram.addValue(field.getValue());
+                    }
+                    idx++;
+                }
+                size++;
+            }
+            iterator.close();
+        } catch (DbException | TransactionAbortedException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     /**
      * Estimates the cost of sequentially scanning the file, given that the cost
      * to read a page is costPerPageIO. You can assume that there are no seeks
      * and that no pages are in the buffer pool.
-     * 
+     * <p>
      * Also, assume that your hard drive can only read entire pages at once, so
      * if the last page of the table only has one tuple on it, it's just as
      * expensive to read as a full page. (Most real hard drives can't
      * efficiently address regions smaller than a page at a time.)
-     * 
+     *
      * @return The estimated cost of scanning the table.
      */
     public double estimateScanCost() {
         // some code goes here
-        return 0;
+        DbFile dbFile = Database.getCatalog().getDatabaseFile(tableId);
+        return dbFile.numPages() * ioCostPerPage;
     }
 
     /**
      * This method returns the number of tuples in the relation, given that a
      * predicate with selectivity selectivityFactor is applied.
-     * 
-     * @param selectivityFactor
-     *            The selectivity of any predicates over the table
+     *
+     * @param selectivityFactor The selectivity of any predicates over the table
      * @return The estimated cardinality of the scan with the specified
-     *         selectivityFactor
+     * selectivityFactor
      */
     public int estimateTableCardinality(double selectivityFactor) {
         // some code goes here
-        return 0;
+        return (int) (selectivityFactor * size);
     }
 
     /**
      * The average selectivity of the field under op.
-     * @param field
-     *        the index of the field
-     * @param op
-     *        the operator in the predicate
-     * The semantic of the method is that, given the table, and then given a
-     * tuple, of which we do not know the value of the field, return the
-     * expected selectivity. You may estimate this value from the histograms.
-     * */
+     *
+     * @param field the index of the field
+     * @param op    the operator in the predicate
+     *              The semantic of the method is that, given the table, and then given a
+     *              tuple, of which we do not know the value of the field, return the
+     *              expected selectivity. You may estimate this value from the histograms.
+     */
     public double avgSelectivity(int field, Predicate.Op op) {
         // some code goes here
-        return 1.0;
+        if (intHistogramMap.containsKey(field)) {
+            return intHistogramMap.get(field).avgSelectivity();
+        }
+        return stringHistogramMap.get(field).avgSelectivity();
     }
 
     /**
      * Estimate the selectivity of predicate <tt>field op constant</tt> on the
      * table.
-     * 
-     * @param field
-     *            The field over which the predicate ranges
-     * @param op
-     *            The logical operation in the predicate
-     * @param constant
-     *            The value against which the field is compared
+     *
+     * @param field    The field over which the predicate ranges
+     * @param op       The logical operation in the predicate
+     * @param constant The value against which the field is compared
      * @return The estimated selectivity (fraction of tuples that satisfy) the
-     *         predicate
+     * predicate
      */
     public double estimateSelectivity(int field, Predicate.Op op, Field constant) {
         // some code goes here
-        return 1.0;
+        if (intHistogramMap.containsKey(field)) {
+            return intHistogramMap.get(field).estimateSelectivity(op, ((IntField) constant).getValue());
+        }
+        return stringHistogramMap.get(field).estimateSelectivity(op, ((StringField) constant).getValue());
     }
 
     /**
      * return the total number of tuples in this table
-     * */
+     */
     public int totalTuples() {
         // some code goes here
-        return 0;
+        return size;
     }
 
 }
